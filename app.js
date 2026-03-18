5c868375685b655dedc3f287a534479935721c90
MilangrebU <mgrebenar@udesa.edu.ar>
2026-03-18 17:03:56 -0300

    fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/main');
 var usersRouter = require('./routes/users');
-var heroesRouter = require('/routes/heroes')
+var heroesRouter = require('./routes/heroes');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/heroes', heroesRouter );
+app.use('/heroes', heroesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
