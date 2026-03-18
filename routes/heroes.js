2d8b80278fade0af43df9e47424d7b40f6af61d3
MilangrebU <mgrebenar@udesa.edu.ar>
2026-03-18 17:32:16 -0300

    sufijos

--- routes/heroes.js
+++ routes/heroes.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('heroes');
 });
 
+router.get('/detalle/id/:id', function(req, res, next){
+    res.send('detalle del heroe del id') + req.paramas.id
+})
+
+
+router.get('/bio/id/:id/ok?', function(req, res, next){
+    res.send('bio del heroe') + req.params.id
+})
+
+
+
+
 module.exports = router;
